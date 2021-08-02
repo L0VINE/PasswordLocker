4cf380a0d500d2fa66d19441049c71b5ab3e9dd1
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-08-02 22:57:47 +0300

    imported unittest method to test

---
+++ user_credentials_test.py
@@ -0,0 +1,15 @@
+import unittest
+import pyperclip
+from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
