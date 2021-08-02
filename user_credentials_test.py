67ba821bdd43ff5bd3d99c0c6ef1f3177fb43bd9
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-08-02 23:00:30 +0300

    added test to show if user information is saved

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -21,3 +21,10 @@ class TestUser(unittest.TestCase):
 		self.assertEqual(self.new_user.first_name,'Mary')
 		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
 		self.assertEqual(self.new_user.password,'pswd100')
+
+        def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
